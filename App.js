289e502517d005f354cba21aaf57f2acf939038e
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-12 14:07:08 +0530

    008. Created nested elements using React (without JSX)

--- App.js
+++ App.js
@@ -3,5 +3,63 @@ const heading = React.createElement(
   { id: "heading", demo: "abc" },
   "Hello World from React!!"
 );
+
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>I'm a h1 tag</h1>
+    </div>
+</div> */
+}
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm a h1 tag")
+  )
+);
+
+{
+  /* <div id="parent">
+      <div id="child">
+          <h1>I'm a h1 tag</h1>
+          <h2>I'm a h2 tag</h2>
+      </div>
+  </div> */
+}
+const parent1 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+  ])
+);
+
+{
+  /* <div id="parent">
+      <div id="child1">
+          <h1>I'm a h1 tag</h1>
+          <h2>I'm a h2 tag</h2>
+      </div>
+      <div id="child2">
+          <h1>I'm a h1 tag</h1>
+          <h2>I'm a h2 tag</h2>
+      </div>
+  </div> */
+}
+const parent2 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent2);
