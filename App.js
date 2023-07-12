47c0dfae8a6f583e8a87ff1d690a2e767513c2ef
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-12 13:44:03 +0530

    007. Adding attributes to React element and css file

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
-const heading = React.createElement("h1", {}, "Hello World from React!!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", demo: "abc" },
+  "Hello World from React!!"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
