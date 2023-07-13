d5b476768f7ed1f526b627240c65172614bf830c
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-13 17:58:05 +0530

    019. Added Element and Component in App.js

--- Ep-03-Laying_the_foundation/App.js
+++ Ep-03-Laying_the_foundation/App.js
@@ -3,68 +3,26 @@ import React from "react";
 // So, to remove the warning we need to import it from "react-dom/client"
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", demo: "abc" },
-  "Hello World from React!!"
+// JSX (transpiled before it reaches the JS Engine) - Parcel - Babel
+// JSX => React.createElement => ReactElement (JS Object) => HTMLElement(render)
+
+// React Element
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Namaste React using JSX
+  </h1>
 );
 
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>I'm a h1 tag</h1>
-    </div>
-</div> */
-}
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm a h1 tag")
-  )
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
-
-{
-  /* <div id="parent">
-      <div id="child">
-          <h1>I'm a h1 tag</h1>
-          <h2>I'm a h2 tag</h2>
-      </div>
-  </div> */
-}
-const parent1 = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ])
-);
-
-{
-  /* <div id="parent">
-      <div id="child1">
-          <h1>I'm a h1 tag</h1>
-          <h2>I'm a h2 tag</h2>
-      </div>
-      <div id="child2">
-          <h1>I'm a h1 tag</h1>
-          <h2>I'm a h2 tag</h2>
-      </div>
-  </div> */
-}
-const parent2 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ]),
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent2);
+
+// root.render(jsxHeading);
+// render only accept react element as argument, so in case of react component,
+// we need to wrap it in angular brackets.
+root.render(<HeadingComponent />);
