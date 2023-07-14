e730f350b9c9a0f301044065633fb08fc1595b66
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-14 11:28:23 +0530

    025. Added Body with search and restaurant component and css

--- Ep-04-FoodApp/App.js
+++ Ep-04-FoodApp/App.js
@@ -22,10 +22,43 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="restaurant logo"
+        src="https://1000logos.net/wp-content/uploads/2023/04/Dominos-logo.png"
+      />
+      <h2>Dominos</h2>
+      <h4>Italian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
