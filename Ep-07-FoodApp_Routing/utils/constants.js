7f03dd8e33d91f4477e1bb339335c0b853ef0e51
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-16 11:58:32 +0530

    036. Initail commit for Ep-7

---
+++ Ep-07-FoodApp_Routing/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_RESTAURANT_LOGO_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const APP_LOGO_URL =
+  "https://png.pngtree.com/element_our/png_detail/20180913/food-point-logo-design-spoon-fork-and-pin-sign-logo-png_94919.jpg";
+
+export const SWIGGY_API_URL =
+  "https://www.swiggy.com/api/seo/getListing?lat=28.67003492726483&lng=77.11469986756225";
