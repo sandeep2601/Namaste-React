110de4d53530db08d0f07c5e297ebe138743791d
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-16 13:43:35 +0530

    040. Created links for Nav menu items using Links

--- Ep-07-FoodApp_Routing/components/Header.js
+++ Ep-07-FoodApp_Routing/components/Header.js
@@ -1,4 +1,5 @@
 import { APP_LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
@@ -8,10 +9,18 @@ export const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
         </ul>
       </div>
     </div>
