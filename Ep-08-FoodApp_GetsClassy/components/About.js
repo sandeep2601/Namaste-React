70be5a42639b56d03e04a0a4b1b6bf026c4b4733
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-17 17:32:05 +0530

    045. Created the class based component

--- Ep-08-FoodApp_GetsClassy/components/About.js
+++ Ep-08-FoodApp_GetsClassy/components/About.js
@@ -1,5 +1,14 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
-  return <div>About Us Page</div>;
+  return (
+    <>
+      <div>About Us Page</div>
+      <User name="Sandeep Verma" location="New Delhi" />
+      <UserClass name="Sandeep Verma" location="New Delhi" />
+    </>
+  );
 };
 
 export default About;
---
+++ Ep-08-FoodApp_GetsClassy/components/UserClass.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class UserClass extends Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: </h2>
+        <h3>Location: </h3>
+        <h4>Contact: sandeepverma</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
