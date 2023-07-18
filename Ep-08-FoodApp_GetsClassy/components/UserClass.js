e18b1e7458cb7325d9ff02fcbccec6e0afd041c9
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-18 22:38:43 +0530

    047. Added the state in class based component

--- Ep-08-FoodApp_GetsClassy/components/UserClass.js
+++ Ep-08-FoodApp_GetsClassy/components/UserClass.js
@@ -3,12 +3,30 @@ import React, { Component } from "react";
 export class UserClass extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count1: 0,
+      count2: 2,
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count1, count2 } = this.state;
     return (
       <div className="user-card">
+        <button
+          onClick={() => {
+            this.setState({
+              count1: this.state.count1 + 1,
+              count2: this.state.count2 + 1,
+            });
+          }}
+        >
+          Increase Count
+        </button>
+        <h2>Count1 : {count1}</h2>
+        <h2>Count2 : {count2}</h2>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: sandeepverma</h4>
