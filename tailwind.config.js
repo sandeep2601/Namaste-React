2178f66e11829028443fc980c860eeb87a01ec44
Sandeep Verma <sandeepverma4321@gmail.com>
2023-07-26 15:23:44 +0530

    076. Created an app store using redux toolkit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // As my project doesn't contain src
   // content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-  content: ["./Ep-11-FoodApp_Data/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["./Ep-12-FoodApp_ReduxToolkit/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
